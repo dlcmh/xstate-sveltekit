a6bffcd4f767c20184e314e5ab93fe4efb647b9c
David Chin <dlcmhd@me.com>
2021-11-15 10:44:18 +0800

    pnpm i -D @sveltejs/adapter-vercel@next

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    // https://github.com/sveltejs/kit/tree/master/packages/adapter-vercel
+    adapter: vercel(),
+
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+  },
 };
 
 export default config;
